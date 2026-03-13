f4de870f29d37d50b024c62a5c6e3ff3e6a1e76a
lui0slpk <itslucky535@gmail.com>
2026-03-13 07:08:47 -0500

    Se agrega un nuevo método

--- src/main/java/com/sena/springpoo/controller/ControllerAdmin.java
+++ src/main/java/com/sena/springpoo/controller/ControllerAdmin.java
@@ -9,6 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class ControllerAdmin {
 
+    @GetMapping("/")
+    public String inicio(){
+        return "Registro";
+    }
+
     @PostMapping("/guardarUsuario")
     public String metodoPost(){
         return "Usuario guardado";
