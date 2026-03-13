8b0c53b1bcb7f3bb88382c0aee11954b5c0361a1
desknoc <juangamering82@gmail.com>
2026-03-13 11:00:18 -0500

    Con esto detectamos el lenguaje del navegador

--- src/main/java/com/sena/springpoo/controller/IdiomaController.java
+++ src/main/java/com/sena/springpoo/controller/IdiomaController.java
@@ -6,4 +6,15 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 public class IdiomaController {
 
+    @GetMapping("/saludo")
+    public String saludo(@RequestHeader("Accept-Language") String idioma){
+
+        if(idioma.startsWith("es")){
+            return "Hola usuario";
+        }else{
+            return "Hello user";
+        }
+
+    }
+
 }
